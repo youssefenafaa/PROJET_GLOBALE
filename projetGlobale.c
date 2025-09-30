0f01efb0e7b653f6d8f46d33db9d8ca880adaceb
youssefenafaa <youssefnafaa2000@gmail.com>
2025-09-30 16:33:25 +0200

    projet globale

---
+++ projetGlobale.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main(){
+
+
+printf("  === SYSTÈME D'ACHAT CLIENT ===\n");
+printf( "1 -----> Gestion du profil client\n");
+printf("2 ----->  Gestion du solde virtuel\n");
+printf("3 -----> Consultation des produits\n");
+printf("4 -----> Effectuer un achat\n");
+printf("5 -----> Mes statistiques\n");
+printf("0 -----> Quitter l'application \n" );
+printf(" Votre choix: ");
+scanf("%d", &choix:);
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+ 
+}
